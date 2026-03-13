4f9b50d36091192cd824a914ab593c3809b7800f
venky2809 <jvenkatesh494@gmail.com>
2026-03-13 17:22:55 +0000

    Update Installation-script.sh

--- Installation-script.sh
+++ Installation-script.sh
@@ -6,11 +6,11 @@ echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.n
 sudo apt update -y
 sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-                  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2026.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-                  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-                              /etc/apt/sources.list.d/jenkins.list > /dev/null
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
